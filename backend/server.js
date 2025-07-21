2fd602b2e2f78eb007966d7ae981fb06b765568e
Terry Griffin <terry.griffin@msutexas.edu>
2025-07-21 11:59:36 -0500

    Deploying changes

--- backend/server.js
+++ backend/server.js
@@ -8,7 +8,7 @@ const checkoutRoutes = require('./routes/checkout');
 const adminRoutes = require('./routes/admin');
 
 const app = express();
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 5055;
 
 app.use(cors());
 app.use(express.json());
