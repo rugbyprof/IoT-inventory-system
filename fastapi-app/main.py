732d1b5c7c09e233210b16571b0c53cbcd013313
Terry Griffin <terry.griffin@msutexas.edu>
2025-07-23 14:13:51 -0500

    Deploying changes

--- fastapi-app/main.py
+++ fastapi-app/main.py
@@ -139,7 +139,8 @@ def login(u: UserLogin):
     cur.execute("SELECT * FROM users WHERE username=%s", (u.username,))
     user = cur.fetchone()
     print(u)
-    print(user["password"])
+    print(f"Table Hash: {user['password'].encode()}")
+    print(f"Input Hash: {u.password.encode()}")
     if not user or not bcrypt.checkpw(u.password.encode(), user["password"].encode()):
         raise HTTPException(status_code=401, detail="Invalid credentials")
     token = create_token(
